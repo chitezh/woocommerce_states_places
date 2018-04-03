d432dba2f028fd6b239388b5d9eb3044853768a6
Ronald Montenegro <rodmontgt@gmail.com>
2018-04-03 14:29:09 -0600

    Update GT.php
    added guatemalan departments

--- trunk/states/GT.php
+++ trunk/states/GT.php
@@ -1,1 +1,35 @@
 
+<?php
+/**
+ * Guatemalan departments
+ *
+ * @author  Ronald Montenegro <rodmontgt@gmail.com>
+ * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+global $states;
+
+$states ['GT' ] = array (
+    'GT-AV' => 'Alta Verapaz',
+    'GT-BV' => 'Baja Verapaz'
+    'GT-CM' => 'Chimaltenango',
+    'GT-CQ' => 'Chiquimula',
+    'GT-PR' => 'El Progreso',  
+    'GT-ES' => 'Escuintla',
+    'GT-GU' => 'Guatemala',
+    'GT-HU' => 'Huehuetenango',
+    'GT-IZ' => 'Izabal',
+    'GT-JA' => 'Jalapa',
+    'GT-JU' => 'Jutiapa',
+    'GT-PE' => 'Petén',
+    'GT-QZ' => 'Quetzaltenango',
+    'GT-QC' => 'Quiché',
+    'GT-RE' => 'Retalhuleu',
+    'GT-SA' => 'Sacatepéquez',
+    'GT-SM' => 'San Marcos',
+    'GT-SR' => 'Santa Rosa',
+    'GT-SO' => 'Sololá',
+    'GT-SU' => 'Suchitepéquez',
+    'GT-TO' => 'Totonicapán',
+    'GT-ZA' => 'Zacapa',
+  );
