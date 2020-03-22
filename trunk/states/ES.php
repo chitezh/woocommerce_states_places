efb9e51e16dd7b9738cacde423a18a1717ce24ad
Javier Esteban <nobnob@gmail.com>
2020-03-22 18:46:16 +0100

    Provinces of Spain

---
+++ trunk/states/ES.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Provinces of Spain
+ *
+ * @author  Javier Esteban <correo@javieresteban.org> | https://javieresteban.org
+ * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states['ES'] = array(
+  '01' => 'Álava/Araba',
+  '02' => 'Albacete',
+  '03' => 'Alicante/Alacant',
+  '04' => 'Almería',
+  '33' => 'Asturias',
+  '05' => 'Ávila',
+  '06' => 'Badajoz',
+  '07' => 'Baleares, Islas/Balears, Illes',
+  '08' => 'Barcelona',
+  '09' => 'Burgos',
+  '10' => 'Cáceres',
+  '11' => 'Cádiz',
+  '39' => 'Cantabria',
+  '12' => 'Castellón/Castelló',
+  '51' => 'Ceuta',
+  '13' => 'Ciudad Real',
+  '14' => 'Córdoba',
+  '15' => 'Coruña, La/Coruña, A',
+  '16' => 'Cuenca',
+  '17' => 'Gerona/Girona',
+  '18' => 'Granada',
+  '19' => 'Guadalajara',
+  '20' => 'Guipúzcoa/Guipuzkoa',
+  '21' => 'Huelva',
+  '22' => 'Huesca',
+  '23' => 'Jaén',
+  '24' => 'León',
+  '25' => 'Lérida/Lleida',
+  '27' => 'Lugo',
+  '28' => 'Madrid',
+  '29' => 'Málaga',
+  '52' => 'Melilla',
+  '30' => 'Murcia',
+  '31' => 'Navarra',
+  '32' => 'Orense/Ourense',
+  '34' => 'Palencia',
+  '35' => 'Palmas, Las',
+  '36' => 'Pontevedra',
+  '26' => 'Rioja, La',
+  '37' => 'Salamanca',
+  '38' => 'Santa Cruz de Tenerife',
+  '40' => 'Segovia',
+  '41' => 'Sevilla',
+  '42' => 'Soria',
+  '43' => 'Tarragona',
+  '44' => 'Teruel',
+  '45' => 'Toledo',
+  '46' => 'Valencia/València',
+  '47' => 'Valladolid',
+  '48' => 'Vizcaya/Bizkaia',
+  '49' => 'Zamora',
+  '50' => 'Zaragoza',
+);
