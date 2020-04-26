da7deb3955af5ff3e084beebfc9f21e1dc04d6ab
Miguel A. Risco-Castillo <mriscoc@gmail.com>
2020-04-26 14:50:33 -0500

    Added Peru - Lima - Callao

---
+++ trunk/places/PE.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * $country places/cities/municipalities/districts
+ * - $count places/cities/municipalities/districts
+ * 
+ * Source: 
+ * - [Please add the source(s) link(s) to check the list of places]
+ * 
+ * @author  Miguel Risco-Castillo <mrisco@accesus.com> | https://accesus.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $places;
+
+$places['PE'] = array(
+  // Uses http://webinei.inei.gob.pe:8080/sisconcode/proyecto/index.htm?proyectoTitulo=UBIGEO&proyectoId=3  
+  'LMA' => array(
+    'Lima'
+    'Ancón'
+    'Ate'
+    'Barranco',
+    'Breña',
+    'Carabayllo',
+    'Chaclacayo',
+    'Chorrillos',
+    'Cieneguilla',
+    'Comas',
+    'El Agustino',
+    'Independencia',
+    'Jesús María',
+    'La Molina',
+    'La Victoria',
+    'Lince',
+    'Los Olivos',
+    'Lurigancho',
+    'Lurin',
+    'Magdalena del Mar',
+    'Pueblo Libre',
+    'Miraflores',
+    'Pachacamac',
+    'Pucusana',
+    'Puente Piedra',
+    'Punta Hermosa',
+    'Punta Negra',
+    'Rímac',
+    'San Bartolo',
+    'San Borja',
+    'San Isidro',
+    'San Juan de Lurigancho',
+    'San Juan de Miraflores',
+    'San Luis',
+    'San Martín de Porres',
+    'San Miguel',
+    'Santa Anita',
+    'Santa María del Mar',
+    'Santa Rosa',
+    'Santiago de Surco',
+    'Surquillo',
+    'Villa El Salvador',
+    'Villa María del Triunfo',
+  ),
+  'CAL' => array(
+    'Callao',
+    'Bellavista',
+    'Carmen de la Legua Reynoso',
+    'La Perla',
+    'La Punta',
+    'Ventanilla',
+    'Mi Perú',
+  ),
+);
---
+++ trunk/states/PE.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * $country states/provinces/regions
+ * - $count states/provinces/regions
+ * 
+ * Source: 
+ * - [Please add the source(s) link(s) to check the list of places]
+ *
+ * @author  Miguel Risco-Castillo <mrisco@accesus.com> | https://accesus.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+$states['PE'] = array(
+// Uses https://www.iso.org/obp/ui/#iso:code:3166:PE
+  'AMA' => 'Amazonas',
+  'ANC' => 'Ancash',
+  'APU' => 'Apurímac',
+  'ARE' => 'Arequipa',
+  'AYA' => 'Ayacucho',
+  'CAJ' => 'Cajamarca',
+  'CUS' => 'CuscoCuzco',
+  'CAL' => 'El Callao',
+  'HUV' => 'Huancavelica',
+  'HUC' => 'Huánuco',
+  'ICA' => 'Ica',
+  'JUN' => 'Junín',
+  'LAL' => 'La Libertad',
+  'LAM' => 'Lambayeque',
+  'LIM' => 'Lima Provincias',
+  'LOR' => 'Loreto',
+  'MDD' => 'Madre de Dios',
+  'MOQ' => 'Moquegua',
+  'LMA' => 'Lima Metropolitana',
+  'PAS' => 'Pasco',
+  'PIU' => 'Piura',
+  'PUN' => 'Puno',
+  'SAM' => 'San Martín',
+  'TAC' => 'Tacna',
+  'TUM' => 'Tumbes',
+  'UCA' => 'Ucayali',
+);
