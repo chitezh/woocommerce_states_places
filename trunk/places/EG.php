76841b95afad6200ee305bc1db56fc794d7fbaa8
yward <33111389+yward@users.noreply.github.com>
2020-05-02 13:38:25 +0200

    Egypt
    adds Egypt

---
+++ trunk/places/EG.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Egyptian States
+ *
+ * @author  Yousif Ward info@yward.net | * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+global $places;
+
+$places['EG'] = array(
+	'CAI' => array(
+		'10th of Ramadan',
+		'1st Settlement',
+		'3rd Settlement',
+		'5th Settlement',
+		'6th of October City',
+		'Abbassia',
+		'Abdeen',
+		'Abu Rawash',
+		'Agouza',
+		'Ain Shams',
+		'Al Manial',
+		'Al Marj',
+		'Al Muqattam',
+		'Al Narges',
+		'Al Rehab',
+		'Al Wayli',
+		'Ard El Lewa',
+		'Bulaq',
+		'Dokki',
+		'Downtown Cairo',
+		'Dream Land',
+		'El Banafseg',
+		'El Hadaba El Wosta',
+		'El Matareya',
+		'El Qobbah',
+		'El Yasmeen',
+		'El-Shorouk',
+		'El-Zaytoun',
+		'Faisal',
+		'Future City',
+		'Garden City',
+		'Gesr elswes',
+		'Haram',
+		'Heliopolis',
+		'Helwan',
+		'Imbaba',
+		'Katameya',
+		'Maadi',
+		'Madinaty',
+		'Masr Al Jadidah',
+		'Masr El Kadema',
+		'Mit Akaba',
+		'Mohandeseen',
+		'Nasr City',
+		'New Giza',
+		'Obour',
+		'Pyramids Gardens (Hadayek al-Ahram)',
+		'Road El Farag',
+		'Sheikh Zayed City',
+		'Sheraton',
+		'Shubra',
+		'Shubra El Kheima',
+		'Smart Village',
+		'Uptown Cairo',
+		'Zamalek'
+
+	),
+	'ALY' => array(
+		'Agamey',
+		'Amreya',
+		'Downtown Alex',
+		'East Alex',
+		'El Gomrok',
+		'El Montaza',
+		'El Sahel'
+
+	),
+	
+);
