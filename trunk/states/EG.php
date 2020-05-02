0ce951de982b7783b183f6d02d21896818e8645b
yward <33111389+yward@users.noreply.github.com>
2020-05-02 13:39:31 +0200

    Adds Egypt

---
+++ trunk/states/EG.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Egyptian States
+ *
+ * @author  Yousif Ward info@yward.net | * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states ['EG' ] = array (
+	'CAI'   => 'Cairo Governorate',
+	'ALY'   => 'Alexandria Governorate',
+);
