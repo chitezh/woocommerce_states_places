cfd046da907dd5241c2e0bf9948bdeae48b9146a
ialwardi <65272556+ialwardi@users.noreply.github.com>
2020-05-13 11:40:46 +0400

    Update and rename XX.php to om.php

---
+++ templates/places/om.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * $country places/cities/municipalities/districts
+ * - $count places/cities/municipalities/districts
+ * 
+ * Source: 
+ * - [Please add the source(s) link(s) to check the list of places]
+ * 
+ * @author  iAlwardi <alwardiamur@gmail.com> 
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $places;
+
+
+$places['OM'] = array(
+  // Replace 'AA' code with the code of the State you want to mapping this places  
+  'MC' => array(
+    // Add all the places of this State, one place per line
+    'Alseeb',
+    'Bawshar',
+    'Matrah',
+    'Muscat',
+    'Quraiat',
+  ),
+  'MN' => array(
+    'Kasab',
+    'Madha',
+    'Daba',
+  ),
+  'BN' => array(
+    'Sohar',
+    'Shinas',
+    'Liwa',
+    'Saham',
+    'Alkhabora',
+    'Alsuwaiq',
+  ),
+  'BS' => array(
+    'Alrustaq',
+    'Wadi Almaawil',
+    'Nakal',
+    'Almusina',
+    'Barka',
+  ),
+  'BU' => array(
+    'Alburimi',
+    'Mahadha',
+  ),
+  'DH' => array(
+    'Ibri',
+    'Dhank',
+    'Yanqul',
+  ),
+  'AD' => array(
+    'Nizwa',
+    'Samail',
+    'Bahla',
+    'Alhamra',
+    'Manah',
+    'Izki',
+    'Adam',
+  ),
+    'NS' => array(
+    'Ibra',
+    'Almudhibi',
+    'Dama Wataien',
+    'Wadi Bni Khalid',
+    'Bidiah',
+    'Alqabil',
+  ),
+    'SS' => array(
+    'Sur',
+    'Jalan Banibu Ali',
+    'Jalan Banibu Hasan',
+    'Musirah',
+    'Alkamil Walwafi',
+  ),
+    'WS' => array(
+    'Hima',
+    'Mahoot',
+    'Alduqum',
+    'Aljazer',
+  ),
+    'DO' => array(
+    'Salalah',
+    'Taqa',
+    'Marbat',
+    'Maqshan',
+    'Thamrait',
+  ),
+);
