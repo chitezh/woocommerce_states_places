1d85f348cc41835b4510987250cf4f49bdda6942
ialwardi <65272556+ialwardi@users.noreply.github.com>
2020-05-13 09:43:43 +0400

    Update and rename XX.php to om.php

---
+++ templates/states/om.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * $country states/provinces/regions
+ * - $count states/provinces/regions
+ * 
+ * Source: 
+ * - [Please add the source(s) link(s) to check the list of places]
+ *
+ * @author  iAlwardi <alwardiamur@gmail.com> 
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
+// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
+$states['OM'] = array(
+  'MC' => 'Muscat',
+  'MN' => 'Musandam',
+  'BN' => 'Al Batinah North',
+  'BS' => 'Al Batinah South',
+  'BU' => 'Al Burimi',
+  'DH' => 'Al Dahira',
+  'AD' => 'Al Dakhilia',
+  'SN' => 'Al Sharqiah North',
+  'SS' => 'Al Sharqiah South',
+  'WS' => 'Al Wosta',
+  'DO' => 'Dhofar',
+);
