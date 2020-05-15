0ffc43c22b77bf4b5fa1b8c7f6ece3cd5f5c7d39
ialwardi <65272556+ialwardi@users.noreply.github.com>
2020-05-15 14:34:15 +0400

    Update om.php

--- templates/states/om.php
+++ templates/states/om.php
@@ -17,15 +17,15 @@ global $states;
 // Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
 // Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
 $states['OM'] = array(
-  'MC' => 'Muscat',
-  'MN' => 'Musandam',
-  'BN' => 'Al Batinah North',
-  'BS' => 'Al Batinah South',
-  'BU' => 'Al Burimi',
-  'DH' => 'Al Dahira',
-  'AD' => 'Al Dakhilia',
-  'SN' => 'Al Sharqiah North',
-  'SS' => 'Al Sharqiah South',
-  'WS' => 'Al Wosta',
-  'DO' => 'Dhofar',
+  'MC' => 'مسقط (Muscat)',
+  'MN' => 'مسندم (Musandam)',
+  'BN' => 'شمال الباطنة (Al Batinah North)',
+  'BS' => 'جنوب الباطنة (Al Batinah South)',
+  'BU' => 'البريمي (Al Burimi)',
+  'DH' => 'الظاهرة (Al Dahira)',
+  'AD' => 'الداخلية (Al Dakhilia)',
+  'SN' => 'شمال الشرقية (Al Sharqiah North)',
+  'SS' => 'جنوب الشرقية (Al Sharqiah South)',
+  'WS' => 'الوسطى (Al Wosta)',
+  'DO' => 'ظفار (Dhofar)',
 );
