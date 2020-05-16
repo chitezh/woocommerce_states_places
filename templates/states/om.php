ffc9e3089a9b2f39061425c29c46023c753f6276
Yordan Soares <38109855+YordanSoares@users.noreply.github.com>
2020-05-16 06:27:49 -0400

    Added metadata and remove comments

--- templates/states/om.php
+++ templates/states/om.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * $country states/provinces/regions
- * - $count states/provinces/regions
+ * Governorates of Oman
+ * - 11 governorates
  * 
  * Source: 
- * - [Please add the source(s) link(s) to check the list of places]
+ * - https://en.wikipedia.org/wiki/Governorates_of_Oman
  *
  * @author  iAlwardi <alwardiamur@gmail.com> 
  * @version 1.0.0
@@ -14,8 +14,6 @@
 
 global $states;
 
-// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
-// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
 $states['OM'] = array(
   'MC' => 'مسقط (Muscat)',
   'MN' => 'مسندم (Musandam)',
