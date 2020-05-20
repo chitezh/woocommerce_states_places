ca4fdae7e20f74a39f6beb17f557470e4a155698
Faisal Basra <faisal.basra@outlook.com>
2020-05-20 19:04:49 +0200

    Added Pakistan Provinces/States

---
+++ trunk/states/PK.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Nigerian states
+ *
+ * @author   Kingsley Ochu <ochu.kc@gmail.com>
+ * @version  1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * Credits: alexasomba
+ */
+
+global $states;
+
+$states ['PK' ] = array (
+	'JK' => 'Azad Kashmir' ,
+	'TA' => 'FATA' ,
+	'GB' => 'Gilgit Baltistan' ,
+	'KP' => 'Khyber Pakhtunkhwa' ,
+	'SD' => 'Sindh',
+	'PB' => 'Punjab',
+	'BA' => 'Balochistan',
+	'IS' => 'Islamabad Capital Territory'
+	);
