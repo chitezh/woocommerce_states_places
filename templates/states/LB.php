4b3fffff108f1de001958656d52b6e2c8a1c3a65
makkehma <66426147+makkehma@users.noreply.github.com>
2020-06-07 09:55:08 -0400

    Create LB.php
    Lebanon Provinces (States or Governorates)

---
+++ templates/states/LB.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Provinces of Lebanon
+ * - 6
+ * 
+ * Source: 
+ * - Liban Post Back End
+ *
+ * @author  Mohamad Ali MAKKEH <makkehma@hotmail.fr> | https://gmartgroup.com/
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
+// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
+$states['LB'] = array(
+  'BA' => 'Beirut',
+  'AS' => 'North Lebanon',
+  'JL' => 'Mount Lebanon',
+  'JA' => 'South Lebanon',
+  'BI' => 'Bikaa',
+  'NB' => 'Nabatieh',
+);
