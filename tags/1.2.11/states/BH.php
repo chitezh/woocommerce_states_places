f88dd83203cb17d97cc75fb136598e724edf3ae2
Yordan Soares <contacto@yordansoar.es>
2020-10-07 18:41:03 -0400

    Tagging 1.2.11 - Oct 07, 2020
    * Added four new countries:
      * 🇦🇺 Australia: States, Territories, and Cities (thanks to [Gopinath N A](https://github.com/Gopinath1728))
      * 🇧🇭 Bahrain: Parishes and Districts (thanks to [Ebrahim Ahmed](https://github.com/Ebrahim7707))
      * 🇵🇾 Paraguay: Departments and Districts.
      * 🇵🇪 Peru: Departments and Districts (thanks to [David Soto](https://github.com/Ddiods))
    * Updated 1 country:
      * 🇪🇸 Spain: Updated keys from States array (thanks to [Javier Esteban](https://profiles.wordpress.org/nobnob/))

---
+++ tags/1.2.11/states/BH.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Governorates of Bahrain
+ * - 4 Governorates
+ * 
+ * Source: 
+ * - https://en.wikipedia.org/wiki/Governorates_of_Bahrain
+ * - https://en.wikipedia.org/wiki/ISO_3166-2:BH
+ *
+ * @author  Ebrahim Ahmed <ebrahim@alraneenlabs.com> | https://alraneenlabs.com/
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+$states['BH'] = array(
+  'BH-13' => _x('Capital', 'Governorate of Bahrain', 'states-cities-and-places-for-woocommerce'),
+  'BH-14' => _x('Southern', 'Governorate of Bahrain', 'states-cities-and-places-for-woocommerce'),
+  'BH-15' => _x('Muharraq', 'Governorate of Bahrain', 'states-cities-and-places-for-woocommerce'),
+  'BH-17' => _x('Northern', 'Governorate of Bahrain', 'states-cities-and-places-for-woocommerce'),
+);
