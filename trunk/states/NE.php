c8665fa6f66e99ea8d0f0b50cb1cc1be263ba170
Ibrahim Nasir <ni1886@gmail.com>
2020-10-27 19:07:06 +0100

    Added Regions and departments of Republic of the Niger
    This is the first entry of the Republic of the Niger. The Regions and Departments represent States and Places.

---
+++ trunk/states/NE.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Nigerien Regions (States)
+ * 
+ * Source: 
+ * - https://en.wikipedia.org/wiki/Regions_of_Niger
+ *
+ * @author  Ibrahim Nasir <ni1886@gmail.com> | https://howdoyou.tech
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+
+$states['NE'] = array(
+
+  'AG' => 'Agadez',
+  'DF' => 'Diffa',
+  'DS' => 'Dosso',
+  'MA' => 'Maradi',
+  'NI' => 'Niamey',
+  'TH' => 'Tahoa',
+  'TL' => 'Tillaberi'
+  'ZI' => 'Zinder'
+);
