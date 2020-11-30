af6261decb5bf827f520099e22992478e7f11797
Ibrahim Nasir <kh4l33l@yahoo.com>
2020-11-30 02:28:20 +0100

    Added Regions and departments of Republic of the Niger (#139)
    * Updated NG Local Governments
    
    - Corrected the Kebbi State Array Name to KE and re-arranged it alphabetically
    - Corrected Garum Mallam LG in Kano state
    -Removed additional 'u' in Matazu LG in Katsina State
    
    * Corrected Sokoto Array Name
    
    Corrected Sokoto Array Name from the mis-match (SO & SK) to SO in both places and states
    
    * Corrected Sokoto Array Name
    
    Corrected Sokoto Array Name from the mis-match (SO & SK) to SO in both places and states
    
    * Added Regions and departments of Republic of the Niger
    
    This is the first entry of the Republic of the Niger. The Regions and Departments represent States and Places.
    
    * Updated NE.php in states
    
    * Update to NE.php
    
    Corrected the formatting of some directorates
    
    Co-authored-by: Ibrahim Nasir <ni1886@gmail.com>

---
+++ trunk/places/NE.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Nigerien Departments (Places)
+ * 
+ * Source: 
+ * - https://en.wikipedia.org/wiki/Departments_of_Niger
+ * 
+ * @author  Ibrahim Nasir <ni1886@gmail.com> | https://howdoyou.tech
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $places;
+
+
+$places['NE'] = array(
+
+  'AG' => array(
+
+    'Arlit',
+    'Bilma',
+    'Tchirozérine',
+    'Agadez'
+  ),
+  'DF' => array(
+    'Diffa',
+    'Maïné-Soroa',
+    "N'Guigmi",
+  ),
+  'DS' => array(
+    'Boboye',
+    'Dogondoutchi',
+    'Dosso',
+    'Gaya',
+    'Loga',
+  ),
+  'MA' => array(
+    'Aguié',
+    'Dakoro',
+    'Guidan-Roumdji',
+    'Madarounfa',
+    'Maradi',
+    'Mayahi',
+    'Tessaoua',
+  ),
+  'NI' => array(
+    'Niamey I',
+    'Niamey II',
+    'Niamey III',
+    'Niamey IV',
+    'Niamey V',
+  ),
+  'TH' => array(
+    'Abalak',
+    "Birni-N'Konni",
+    'Bouza',
+    'Illéla',
+    'Kéita',
+    'Madaoua',
+    'Tahoua',
+    'Tchin-Tabaraden',
+  ),
+  'TL' => array(
+    'Filingué',
+    'Kollo',
+    'Ouallam',
+    'Say',
+    'Téra',
+    'Tillabéri',
+  ),
+  'ZI' => array(
+    'Gouré',
+    'Magaria',
+    'Matameye',
+    'Mirriah',
+    'Tanout',
+    'Zinder'
+  ),
+);
