a22e393875fb081a5914a4a876a2e4f2371673a3
Marlon Dyce <65045340+marlonc7c@users.noreply.github.com>
2020-12-12 17:02:41 -0400

    Update TT.php

--- trunk/states/TT.php
+++ trunk/states/TT.php
@@ -14,23 +14,7 @@
 
 global $states;
 
-$states['TT'] = array(
-  'Tobago' => 'Tobago',
-  'Arima' => 'Arima',
-  'Chaguanas' => 'Chaguanas',
-  'Couva/Tabaquite/Talparo' => 'Couva/Tabaquite/Talparo',
-  'Diego Martin' => 'Diego Martin',
-  'Mayaro/Rio Claro' => 'Mayaro/Rio Claro',
-  'Penal/Debe' => 'Penal/Debe',
-  'Point Fortin' => 'Point Fortin',
-  'Port of Spain' => 'Port of Spain',
-  'Princes Town' => 'Princes Town',
-  'San Fernando' => 'San Fernando',
-  'San Juan/Laventille' => 'San Juan/Laventille',
-  'Sangre Grande' => 'Sangre Grande',
-  'Siparia' => 'Siparia',
-  'Tunapuna/Piarco' => 'Tunapuna/Piarco'
-);
+$states['TT'] = array();
 
 // Use this filter to handle the Borough / City / Regional Corporations of Trinidad & Tobago
 $states['TT'] = apply_filters('scpwoo_custom_states_tt', $states['TT']);
