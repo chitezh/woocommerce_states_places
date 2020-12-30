1cb743d0857218e5b1a01221378c4982609435fd
Yordan Soares <contacto@yordansoar.es>
2020-12-30 02:45:58 -0400

    Add Cities and Districts of Bangladesh
    Cities of Bangladesh
    - 1035 cities
    
    Districts of Bangladesh
    - 65 districts
    
    Thanks to S.M. Mehdi Akram (https://www.royaltechbd.com)

---
+++ trunk/states/BD.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Districts of Bangladesh
+ * - 65 districts
+ *
+ * @author   S.M. Mehdi Akram <mehdi@royaltechbd.com> | https://www.royaltechbd.com
+ * @version  1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states['BD'] = array(
+	'dhaka' => __('Dhaka', 'states-cities-and-places-for-woocommerce'),
+	'faridpur' => __('Faridpur', 'states-cities-and-places-for-woocommerce'),
+	'gazipur' => __('Gazipur', 'states-cities-and-places-for-woocommerce'),
+	'gopalganj' => __('Gopalganj', 'states-cities-and-places-for-woocommerce'),
+	'kishoreganj' => __('Kishoreganj', 'states-cities-and-places-for-woocommerce'),
+	'madaripur' => __('Madaripur', 'states-cities-and-places-for-woocommerce'),
+	'manikganj' => __('Manikganj', 'states-cities-and-places-for-woocommerce'),
+	'munshiganj' => __('Munshiganj', 'states-cities-and-places-for-woocommerce'),
+	'narayanganj' => __('Narayanganj', 'states-cities-and-places-for-woocommerce'),
+	'narsingdi' => __('Narsingdi', 'states-cities-and-places-for-woocommerce'),
+	'rajbari' => __('Rajbari', 'states-cities-and-places-for-woocommerce'),
+	'shariatpur' => __('Shariatpur', 'states-cities-and-places-for-woocommerce'),
+	'tangail' => __('Tangail', 'states-cities-and-places-for-woocommerce'),
+	'barisal' => __('Barisal', 'states-cities-and-places-for-woocommerce'),
+	'barguna' => __('Barguna', 'states-cities-and-places-for-woocommerce'),
+	'bhola' => __('Bhola', 'states-cities-and-places-for-woocommerce'),
+	'jhalakati' => __('Jhalakati', 'states-cities-and-places-for-woocommerce'),
+	'patuakhali' => __('Patuakhali', 'states-cities-and-places-for-woocommerce'),
+	'pirojpur' => __('Pirojpur', 'states-cities-and-places-for-woocommerce'),
+	'bandarban' => __('Bandarban', 'states-cities-and-places-for-woocommerce'),
+	'brahmanbaria' => __('Brahmanbaria', 'states-cities-and-places-for-woocommerce'),
+	'chandpur' => __('Chandpur', 'states-cities-and-places-for-woocommerce'),
+	'chittagong' => __('Chittagong', 'states-cities-and-places-for-woocommerce'),
+	'comilla' => __('Comilla', 'states-cities-and-places-for-woocommerce'),
+	'coxsbazar' => __('Coxs Bazar', 'states-cities-and-places-for-woocommerce'),
+	'feni' => __('Feni', 'states-cities-and-places-for-woocommerce'),
+	'khagrachhari' => __('Khagrachhari', 'states-cities-and-places-for-woocommerce'),
+	'lakshmipur' => __('Lakshmipur', 'states-cities-and-places-for-woocommerce'),
+	'noakhali' => __('Noakhali', 'states-cities-and-places-for-woocommerce'),
+	'rangamati' => __('Rangamati', 'states-cities-and-places-for-woocommerce'),
+	'jamalpur' => __('Jamalpur', 'states-cities-and-places-for-woocommerce'),
+	'mymensingh' => __('Mymensingh', 'states-cities-and-places-for-woocommerce'),
+	'netrakona' => __('Netrakona', 'states-cities-and-places-for-woocommerce'),
+	'sherpur' => __('Sherpur', 'states-cities-and-places-for-woocommerce'),
+	'bagerhat' => __('Bagerhat', 'states-cities-and-places-for-woocommerce'),
+	'chuadanga' => __('Chuadanga', 'states-cities-and-places-for-woocommerce'),
+	'jessore' => __('Jessore', 'states-cities-and-places-for-woocommerce'),
+	'jhenaidah' => __('Jhenaidah', 'states-cities-and-places-for-woocommerce'),
+	'khulna' => __('Khulna', 'states-cities-and-places-for-woocommerce'),
+	'kushtia' => __('Kushtia', 'states-cities-and-places-for-woocommerce'),
+	'magura' => __('Magura', 'states-cities-and-places-for-woocommerce'),
+	'meherpur' => __('Meherpur', 'states-cities-and-places-for-woocommerce'),
+	'narail' => __('Narail', 'states-cities-and-places-for-woocommerce'),
+	'satkhira' => __('Satkhira', 'states-cities-and-places-for-woocommerce'),
+	'bogra' => __('Bogra', 'states-cities-and-places-for-woocommerce'),
+	'chapainawabganj' => __('Chapai Nawabganj', 'states-cities-and-places-for-woocommerce'),
+	'jaipurhat' => __('Jaipurhat', 'states-cities-and-places-for-woocommerce'),
+	'naogaon' => __('Naogaon', 'states-cities-and-places-for-woocommerce'),
+	'natore' => __('Natore', 'states-cities-and-places-for-woocommerce'),
+	'pabna' => __('Pabna', 'states-cities-and-places-for-woocommerce'),
+	'rajshahi' => __('Rajshahi', 'states-cities-and-places-for-woocommerce'),
+	'sirajganj' => __('Sirajganj', 'states-cities-and-places-for-woocommerce'),
+	'dinajpur' => __('Dinajpur', 'states-cities-and-places-for-woocommerce'),
+	'gaibandha' => __('Gaibandha', 'states-cities-and-places-for-woocommerce'),
+	'kurigram' => __('Kurigram', 'states-cities-and-places-for-woocommerce'),
+	'lalmonirhat' => __('Lalmonirhat', 'states-cities-and-places-for-woocommerce'),
+	'nilphamari' => __('Nilphamari', 'states-cities-and-places-for-woocommerce'),
+	'panchagarh' => __('Panchagarh', 'states-cities-and-places-for-woocommerce'),
+	'rangpur' => __('Rangpur', 'states-cities-and-places-for-woocommerce'),
+	'thakurgaon' => __('Thakurgaon', 'states-cities-and-places-for-woocommerce'),
+	'habiganj' => __('Habiganj', 'states-cities-and-places-for-woocommerce'),
+	'maulvibazar' => __('Maulvi Bazar', 'states-cities-and-places-for-woocommerce'),
+	'sunamganj' => __('Sunamganj', 'states-cities-and-places-for-woocommerce'),
+	'sylhet' => __('Sylhet', 'states-cities-and-places-for-woocommerce'),
+	'saver' => __('Saver', 'states-cities-and-places-for-woocommerce'),
+);
+
+// Use this filter to handle the Districts of Bangladesh
+$states['BD'] = apply_filters('scpwoo_custom_states_bd', $states['BD']);
