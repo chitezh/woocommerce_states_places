1acd49da17fb197a6487ccba2967e19c858c681d
5edmat <s.haroon@5edmat.com>
2021-02-10 05:56:17 +0200

    Update JO.php
    Added Author info

--- templates/places/JO.php
+++ templates/places/JO.php
@@ -7,7 +7,7 @@
  * Source: 
  * - [Please add the source(s) link(s) to check the list of places]
  * 
- * @author  Your Name Here <yourname@here.com> | https://yourwebsite.com
+ * @author  Saed A. Haroon <s.haroon@5edmat.com> | https://5edmat.com
  * @version 1.0.0
  * @license http://www.gnu.org/licenses/gpl-2.0.html
  */
