72cc04a77d9490334b6498a7d4e62cea579f7c6a
5edmat <s.haroon@5edmat.com>
2021-02-15 23:23:37 +0200

    Update JO.php

--- templates/places/JO.php
+++ templates/places/JO.php
@@ -5,7 +5,7 @@
  * - $count places/cities/municipalities/districts
  * 
  * Source: 
- * - [Please add the source(s) link(s) to check the list of places]
+ * - [https://en.wikipedia.org/wiki/List_of_cities_in_Jordan]
  * 
  * @author  Saed A. Haroon <s.haroon@5edmat.com> | https://5edmat.com
  * @version 1.0.0
