a378fc99838866459c665da78e3e9a16e582c94d
dibesraj <57431059+dibesraj@users.noreply.github.com>
2021-04-13 08:50:17 +1000

    Update NP.php

--- trunk/places/NP.php
+++ trunk/places/NP.php
@@ -1,1 +1,37 @@
+<?php
 
+/**
+ * $country places/cities/municipalities/districts
+ * - $count places/cities/municipalities/districts
+ * 
+ * Source: 
+ * - [Please add the source(s) link(s) to check the list of places]
+ * 
+ * @author  Your Name Here <yourname@here.com> | https://yourwebsite.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $places;
+
+// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
+// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
+$places['XX'] = array(
+  // Replace 'AA' code with the code of the State you want to mapping this places  
+  'AA' => array(
+    // Add all the places of this State, one place per line
+    'Location 1',
+    'Location 2',
+    'Location 3',
+  ),
+  'BB' => array(
+    'Location 1',
+    'Location 2',
+    'Location 3',
+  ),
+  'CC' => array(
+    'Location 1',
+    'Location 2',
+    'Location 3',
+  ),
+);
