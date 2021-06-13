8e7d6b9cf008bcddb8d7f03c9dbfad4f07359cb6
Yordan Soares <38109855+YordanSoares@users.noreply.github.com>
2021-06-13 12:59:46 -0400

    Update style and comments
    - Remove explaining comments
    - Add location info
    - Reordered gobernorates
    - Replace double quotes for single quotes
    - Fix indent.

--- trunk/states/JO.php
+++ trunk/states/JO.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * $country states/provinces/regions
- * - $count states/provinces/regions
+ * Governorates of Jordan
+ * - 12 governorates
  * 
  * Source: 
  * - [https://en.wikipedia.org/wiki/List_of_cities_in_Jordan]
@@ -14,30 +14,17 @@
 
 global $states;
 
-// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
-// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
 $states['JO'] = array(
-  // Some countries like Denmark or Belgium do not use the state field, 
-  // if your country doesn't use it either, leave the array empty.
-  // E.g. $states['DK'] = array();
-
-  // Otherwise, if the state field is used in your country:
-  // Add a State Code to each State Name using a logic sense. 
-  // E.g. 'FA' => 'Florida'
-  // You can use any standar like ISO, ANSI, etc. There is no an mandatory standar or system and you can use your own.
-  // Please see the U.S. state abbreviations to see an example:
-  // https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations
-  'AJ' => 'Ajloun',
-  'BA' => 'Al-Balqa',
-  'AQ' => 'Aqaba',
-  'AM' => 'Amman',
-  'JA' => 'Jerash',
-  'IR' => 'Irbid',
-  'KA' => 'Al-Karak',
-  'MA' => "Ma'an",
-  'MD' => 'Madaba',
-  'MF' => 'Mafraq',
-  'TA' => 'Tafilah',
-  'ZA' => 'Zarqa',
-
+	'AM' => 'Amman',
+	'AJ' => 'Ajloun',
+	'BA' => 'Al-Balqa',
+	'AQ' => 'Aqaba',
+	'IR' => 'Irbid',
+	'JA' => 'Jerash',
+	'KA' => 'Al-Karak',
+	'MA' => "Ma'an",
+	'MD' => 'Madaba',
+	'MF' => 'Mafraq',
+	'TA' => 'Tafilah',
+	'ZA' => 'Zarqa',
 );
