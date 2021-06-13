d7c63831405c10dd92f1b4e44fa692f58991a713
5edmat <s.haroon@5edmat.com>
2021-06-13 20:09:57 +0300

    Added Jordan Country Governorate's / Cities / Districts (#167)
    * Added Country
    
    I have added jordan states and cities
    
    * Update JO.php
    
    Added Author info
    
    * Update JO.php
    
    * Restore templates
    
    * Move Jordan files to /trunk
    
    * Update style and comments
    
    - Remove explaining comments
    - Add location info
    - Reordered gobernorates
    - Replace double quotes for single quotes
    - Fix indent.
    
    * Add filter hooks to handle the Governorates and Cities of Jordan
    
    Co-authored-by: Yordan Soares <38109855+YordanSoares@users.noreply.github.com>

---
+++ trunk/places/JO.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * Cities of Jordan
+ * - 50 cities
+ * 
+ * Source: 
+ * - [https://en.wikipedia.org/wiki/List_of_cities_in_Jordan]
+ * 
+ * @author  Saed A. Haroon <s.haroon@5edmat.com> | https://5edmat.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $places;
+
+$places['JO'] = array(
+	// Amman
+	'AM' => array(
+		'Amman',
+		'Al-Quwaysimah',
+		'Tila Al-Ali',
+		'Wadi As-Ser',
+		'Al-Jubayhah',
+		'Khuraybat As-Suq',
+		'Sahab',
+		'Suwaylih',
+		'Umm Qushayr',
+		'Al-Jizah',
+		'Marj Al-Hamam',
+		'Askan Abu Nushayr',
+		'Shafa Badran',
+		'Naour',
+	),
+	// Ajloun
+	'AJ' => array(
+		'Anjara',
+		'Kunfranjah',
+	),
+	// Al-Balqa
+	'BA' => array(
+		'As-Salt',
+		'Al-Baq\'a',
+		'Ayn Al-Basha',
+	),
+	// Aqaba
+	'AQ' => array(
+		'Aqaba',
+	),
+	// Irbid
+	'IR' => array(
+		'Irbid',
+		'Ar Ramtha',
+		'Aydoun',
+		'Ash-Sarih',
+		'Bayt Ras',
+		'Al Husn',
+		'At-Turrah',
+		'Der Abi Saeed',
+		'No\'ayymeh',
+		'AshShajarah',
+		'Muhayyam al-Ashhahid Azmi',
+		'Al-Mashariqah',
+		'Huwwarah',
+		'Kafr Yooba',
+		'Krayymeh',
+	),
+	// Jerash
+	'JA' => array(
+		'Jerash',
+	),
+	// Al-Karak
+	'KA' => array(
+		'Karak',
+		'Mu\'tah',
+		'Ghawr Ash-Shafi',
+	),
+	// Ma'an
+	'MA' => array(
+		'Ma\'an',
+	),
+	// Madaba
+	'MD' => array(
+		'Madaba',
+	),
+	// Mafraq
+	'MF' => array(
+		'Mafraq',
+		'Zaatari Refugee Camp',
+	),
+	// Tafilah
+	'TA' => array(
+		'Tafilah',
+	),
+	// Zarqa
+	'ZA' => array(
+		'Zarqa',
+		'Russeifa',
+		'Ad-Dulayl',
+		'Al-Hashimiyah',
+		'Muhayyam Hatin',
+		'Azraq Refugee Camp',
+	),
+);
+
+// Use this filter to handle the Cities of Jordan
+$places['JO'] = apply_filters('scpwoo_custom_places_jo', $places['JO']);
---
+++ trunk/states/JO.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Governorates of Jordan
+ * - 12 governorates
+ * 
+ * Source: 
+ * - [https://en.wikipedia.org/wiki/List_of_cities_in_Jordan]
+ *
+ * @author  Saed A. Haroon <s.haroon@5edmat.com> | https://5edmat.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+$states['JO'] = array(
+	'AM' => 'Amman',
+	'AJ' => 'Ajloun',
+	'BA' => 'Al-Balqa',
+	'AQ' => 'Aqaba',
+	'IR' => 'Irbid',
+	'JA' => 'Jerash',
+	'KA' => 'Al-Karak',
+	'MA' => "Ma'an",
+	'MD' => 'Madaba',
+	'MF' => 'Mafraq',
+	'TA' => 'Tafilah',
+	'ZA' => 'Zarqa',
+);
+
+// Use this filter to handle the Governorates of Jordan
+$states['JO'] = apply_filters('scpwoo_custom_states_jo', $states['JO']);
