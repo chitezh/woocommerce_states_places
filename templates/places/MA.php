a36a44bad5453739ad3190e363a5ce4a9158e16f
El Mehdi Mouhajer <elmehdimouahjer@gmail.com>
2021-09-18 03:05:46 +0100

    add MA support & citys of the folowing states : Casablanca-Settat, Béni Mellal-Khénifra, Drâa-Tafilalet

---
+++ templates/places/MA.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * $country places/cities/municipalities/districts
+ * - $count places/cities/municipalities/districts
+ * 
+ * Source: 
+ * - [Please add the source(s) link(s) to check the list of places]
+ * 
+ * @author  Your Name Here <yourname@here.com> | https://yourwebsite.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $places;
+
+// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
+// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
+$places['MA'] = array(
+  // Replace 'AA' code with the code of the State you want to mapping this places  
+  '06' => array(
+    // Add all the places of this State, one place per line
+    'Casablanca',
+    'Mohammedia',
+    'El Jadid',
+    'Settat',
+    'Barrechid',
+    'Bouskoura',
+    'Moulay Abdallah',
+    'Aïn Harrouda',
+    'Benslimane',
+    'Sidi Bennour',
+    'Beni Yakhlef',
+    'Ad Darwa',
+    'Azemmour',
+    'Bouznika',
+    'Mediouna',
+    'Bou Ahmed',
+    'Tit Mellil',
+    'Douar Oulad Hssine',
+    'Sidi Smai’il',
+    'Oulad Hammou',
+    'Oulad Said',
+    'Douar Olad. Salem',
+    'Douar Ouled Ayad',
+    'Zawyat an Nwaçer'
+  ),
+  '05' => array(
+    'Kouribga',
+    'Béni Mellal',
+    'Khénifra',
+    'Al Fqih Ben Çalah',
+    'Oued Zem',
+    'Qasbat Tadla',
+    'Boujad',
+    'Mrirt',
+    'Aziylal',
+    'Aguelmous',
+    'Oulad Zemam',
+    'Dar Ould Zidouh',
+    'Demnat',
+    'Douar Oulad Aj-jabri',
+    'Oulad Yaïch',
+    'Zawyat ech Cheïkh',
+    'Ouaoula',
+    'Ayt Mohamed'
+  ),
+  '08' => array(
+    'Errachidia',
+    'Warzat',
+    'Midalt',
+    'Tineghir',
+    'Zagora',
+    'Arfoud',
+    'Skoura'
+  ),
+);
