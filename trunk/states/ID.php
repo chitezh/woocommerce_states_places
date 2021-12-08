7918b4cfb5f54b1b7ca8e40c91f0a47cc1ce2659
ahnaffauzanorg <93607552+ahnaffauzanorg@users.noreply.github.com>
2021-12-09 06:37:16 +0800

    Update ID.php

--- trunk/states/ID.php
+++ trunk/states/ID.php
@@ -2,32 +2,51 @@
 
 /**
  * $country states/provinces/regions
- * - $count states/provinces/regions
+ * - 32 states/provinces/regions
  * 
  * Source: 
- * - [Please add the source(s) link(s) to check the list of places]
+ * - https://id.wikipedia.org/wiki/Provinsi_di_Indonesia
  *
- * @author  Your Name Here <yourname@here.com> | https://yourwebsite.com
+ * @author  Ahnaf Fauzan <ahnaffauzanorg@gmail.com> | https://www.instagram.com/ahnaffauzanorg/
  * @version 1.0.0
  * @license http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 global $states;
 
-// Replace 'XX' code for your Country Code. E.g. 'NG' for Nigeria, 'CO' for Colombia, 'VE' for Venezuela, etc.
-// Please refer to WooCommerce country codes : https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
-$states['XX'] = array(
-  // Some countries like Denmark or Belgium do not use the state field, 
-  // if your country doesn't use it either, leave the array empty.
-  // E.g. $states['DK'] = array();
-
-  // Otherwise, if the state field is used in your country:
-  // Add a State Code to each State Name using a logic sense. 
-  // E.g. 'FA' => 'Florida'
-  // You can use any standar like ISO, ANSI, etc. There is no an mandatory standar or system and you can use your own.
-  // Please see the U.S. state abbreviations to see an example:
-  // https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations
-  'AA' => 'State 1',
-  'BB' => 'State 2',
-  'CC' => 'State 3',
+$states['ID'] = array(
+  'AC' => 'Aceh',
+  'SU' => 'Sumatra Utara',
+  'SB' => 'Sumatra Barat',
+  'RI' => 'Riau',
+  'KR' => 'Kepulauan Riau',
+  'JA' => 'Jambi',
+  'BE' => 'Bengkulu',
+  'SS' => 'Sumatra Selatan',
+  'BB' => 'Kepulauan Bangka Belitung',
+  'LA' => 'Lampung',
+  'JK' => 'Daerah Khusus Ibukota Jakarta',
+  'BT' => 'Banten',
+  'JB' => 'Jawa Barat',
+  'JT' => 'Jawa Tengah',
+  'YO' => 'Daerah Istimewa Yogyakarta',
+  'JI' => 'Jawa Timur',
+  'BA' => 'Bali',
+  'NB' => 'Nusa Tenggara Barat',
+  'NT' => 'Nusa Tenggara Timur',
+  'KB' => 'Kalimantan Barat',
+  'KT' => 'Kalimantan Tengah',
+  'KS' => 'Kalimantan Selatan',
+  'KI' => 'Kalimantan Timur',
+  'KU' => 'Kalimantan Utara',
+  'SA' => 'Sulawesi Utara',
+  'GO' => 'Gorontalo',
+  'ST' => 'Sulawesi Tengah',
+  'SR' => 'Sulawesi Barat',
+  'SN' => 'Sulawesi Selatan',
+  'SG' => 'Sulawesi Tenggara',
+  'MU' => 'Maluku Utara',
+  'MA' => 'Maluku',
+  'PB' => 'Papua Barat',
+  'PA' => 'Papua',
 );
