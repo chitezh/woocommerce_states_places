bc685d62f5c5149b369c566d4679d86e7837a7ab
David Webb Espiritu <webbteche@gmail.com>
2021-12-16 18:01:08 +0800

    First Layout
    Layout the base province.

---
+++ trunk/places/PH.php
@@ -0,0 +1,427 @@
+<?php
+
+/**
+ * Cities and Municipalities of the Philippines
+ * - 146 Cities & 1,488 Municipalities
+ *
+ * Source:
+ * - https://psa.gov.ph/classification/psgc/?q=psgc/provinces
+ *
+ * @author  David Webb Espiritu <webbteche@gmail.com> | https://yourwebsite.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $places;
+
+$places['PH'] = array(
+  // Abra
+  'ABR' => array(
+    'Location 1',
+  ),
+
+  // Agusan del Norte
+  'AGN' => array(
+    'Location 1',
+  ),
+
+  // Agusan del Sur
+  'AGS' => array(
+    'Location 1',
+  ),
+
+  // Aklan
+  'AKL' => array(
+    'Location 1',
+  ),
+
+  // Albay
+  'ALB' => array(
+    'Location 1',
+  ),
+
+  // Antique
+  'ANT' => array(
+    'Location 1',
+  ),
+
+  // Apayao
+  'APA' => array(
+    'Location 1',
+  ),
+
+  // Aurora
+  'AUR' => array(
+    'Location 1',
+  ),
+
+  // Basilan
+  'BAS' => array(
+    'Location 1',
+  ),
+
+  // Bataan
+  'BAN' => array(
+    'Location 1',
+  ),
+
+  // Batanes
+  'BTN' => array(
+    'Location 1',
+  ),
+
+  // Batangas
+  'BTG' => array(
+    'Location 1',
+  ),
+
+  // Benguet
+  'BEN' => array(
+    'Location 1',
+  ),
+
+  // Biliran
+  'BIL' => array(
+    'Location 1',
+  ),
+
+  // Bohol
+  'BOH' => array(
+    'Location 1',
+  ),
+
+  // Bukidnon
+  'BUK' => array(
+    'Location 1',
+  ),
+
+  // Bulacan
+  'BUL' => array(
+    'Location 1',
+  ),
+
+  // Cagayan
+  'CAG' => array(
+    'Location 1',
+  ),
+
+  // Camarines Norte
+  'CAN' => array(
+    'Location 1',
+  ),
+
+  // Camarines Sur
+  'CAS' => array(
+    'Location 1',
+  ),
+
+  // Camiguin
+  'CAM' => array(
+    'Location 1',
+  ),
+
+  // Capiz
+  'CAP' => array(
+    'Location 1',
+  ),
+
+  // Catanduanes
+  'CAT' => array(
+    'Location 1',
+  ),
+
+  // Cavite
+  'CAV' => array(
+    'Location 1',
+  ),
+
+  // Cebu
+  'CEB' => array(
+    'Location 1',
+  ),
+
+  // Compostela Valley
+  'COM' => array(
+    'Location 1',
+  ),
+
+  // Cotabato
+  'NCO' => array(
+    'Location 1',
+  ),
+
+  // Davao del Norte
+  'DAV' => array(
+    'Location 1',
+  ),
+
+  // Davao del Sur
+  'DAS' => array(
+    'Location 1',
+  ),
+
+  // Davao Occidental
+  'DAC' => array(
+    'Location 1',
+  ),
+
+  // Davao Oriental
+  'DAO' => array(
+    'Location 1',
+  ),
+
+  // Dinagat Islands
+  'DIN' => array(
+    'Location 1',
+  ),
+
+  // Eastern Samar
+  'EAS' => array(
+    'Location 1',
+  ),
+
+  // Guimaras
+  'GUI' => array(
+    'Location 1',
+  ),
+
+  // Ifugao
+  'IFU' => array(
+    'Location 1',
+  ),
+
+  // Ilocos Norte
+  'ILN' => array(
+    'Location 1',
+  ),
+
+  // Ilocos Sur
+  'ILS' => array(
+    'Location 1',
+  ),
+
+  // Iloilo
+  'ILI' => array(
+    'Location 1',
+  ),
+
+  // Isabela
+  'ISA' => array(
+    'Location 1',
+  ),
+
+  // Kalinga
+  'KAL' => array(
+    'Location 1',
+  ),
+
+  // La Union
+  'LUN' => array(
+    'Location 1',
+  ),
+
+  // Laguna
+  'LAG' => array(
+    'Location 1',
+  ),
+
+  // Lanao del Norte
+  'LAN' => array(
+    'Location 1',
+  ),
+
+  // Lanao del Sur
+  'LAS' => array(
+    'Location 1',
+  ),
+
+  // Leyte
+  'LEY' => array(
+    'Location 1',
+  ),
+
+  // Maguindanao
+  'MAG' => array(
+    'Location 1',
+  ),
+
+  // Marinduque
+  'MAD' => array(
+    'Location 1',
+  ),
+
+  // Masbate
+  'MAS' => array(
+    'Location 1',
+  ),
+
+  // Misamis Occidental
+  'MSC' => array(
+    'Location 1',
+  ),
+
+  // Misamis Oriental
+  'MSR' => array(
+    'Location 1',
+  ),
+
+  // Mountain Province
+  'MOU' => array(
+    'Location 1',
+  ),
+
+  // Negros Occidental
+  'NEC' => array(
+    'Location 1',
+  ),
+
+  // Negros Oriental
+  'NER' => array(
+    'Location 1',
+  ),
+
+  // Northern Samar
+  'NSA' => array(
+    'Location 1',
+  ),
+
+  // Nueva Ecija
+  'NUE' => array(
+    'Location 1',
+  ),
+
+  // Nueva Vizcaya
+  'NUV' => array(
+    'Location 1',
+  ),
+
+  // Occidental Mindoro
+  'MDC' => array(
+    'Location 1',
+  ),
+
+  // Oriental Mindoro
+  'MDR' => array(
+    'Location 1',
+  ),
+
+  // Palawan
+  'PLW' => array(
+    'Location 1',
+  ),
+
+  // Pampanga
+  'PAM' => array(
+    'Location 1',
+  ),
+
+  // Pangasinan
+  'PAN' => array(
+    'Location 1',
+  ),
+
+  // Quezon
+  'QUE' => array(
+    'Location 1',
+  ),
+
+  // Quirino
+  'QUI' => array(
+    'Location 1',
+  ),
+
+  // Rizal
+  'RIZ' => array(
+    'Location 1',
+  ),
+
+  // Romblon
+  'ROM' => array(
+    'Location 1',
+  ),
+
+  // Samar
+  'WSA' => array(
+    'Location 1',
+  ),
+
+  // Sarangani
+  'SAR' => array(
+    'Location 1',
+  ),
+
+  // Siquijor
+  'SIQ' => array(
+    'Location 1',
+  ),
+
+  // Sorsogon
+  'SOR' => array(
+    'Location 1',
+  ),
+
+  // South Cotabato
+  'SCO' => array(
+    'Location 1',
+  ),
+
+  // Southern Leyte
+  'SLE' => array(
+    'Location 1',
+  ),
+
+  // Sultan Kudarat
+  'SUK' => array(
+    'Location 1',
+  ),
+
+  // Sulu
+  'SLU' => array(
+    'Location 1',
+  ),
+
+  // Surigao del Norte
+  'SUN' => array(
+    'Location 1',
+  ),
+
+  // Surigao del Sur
+  'SUR' => array(
+    'Location 1',
+  ),
+
+  // Tarlac
+  'TAR' => array(
+    'Location 1',
+  ),
+
+  // Tawi-Tawi
+  'TAW' => array(
+    'Location 1',
+  ),
+
+  // Zambales
+  'ZMB' => array(
+    'Location 1',
+  ),
+
+  // Zamboanga del Norte
+  'ZAN' => array(
+    'Location 1',
+  ),
+
+  // Zamboanga del Sur
+  'ZAS' => array(
+    'Location 1',
+  ),
+
+  // Zamboanga Sibugay
+  'ZSI' => array(
+    'Location 1',
+  ),
+
+  // Metro Manila
+  '00' => array(
+    'Location 1',
+  ),
+);
